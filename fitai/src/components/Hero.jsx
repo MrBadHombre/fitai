43d17f4b8a1854db71d618ea2a7321f194f507ea
Michael <l1ghtdev.88@gmail.com>
2023-10-11 21:39:25 -0700

    first commit

---
+++ fitai/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Hero.module.css'
+
+const Hero = () => {
+  return (
+    <>
+      <div className={`${styles.heroTextBlock}`}>
+        <h5 className={styles.heroText}>Hello my name is</h5>
+        <h1 className={styles.heroText}>Michael Margrave.</h1>
+        <h1 className={styles.heroText}>I design stuff for the web.</h1>
+      </div>
+    </>
+  )
+}
+
+export default Hero
